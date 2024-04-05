056c4191979764f726e20a904e74ae30f8bbcb62
IngenieroJosser <133165583+IngenieroJosser@users.noreply.github.com>
2024-04-04 22:22:32 -0700

    creacion de la landing de experiencias

---
+++ src/components/experiences/Experiences.jsx
@@ -0,0 +1,11 @@
+import '../../style/scss/experiences/_experiences.scss'
+
+const Experiences = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Experiences
