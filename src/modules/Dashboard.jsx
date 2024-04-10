18af190d843bcfeb60b301e2c5d292df8f900c14
IngenieroJosser <133165583+IngenieroJosser@users.noreply.github.com>
2024-04-10 15:33:38 -0700

    creacion del dashboard

---
+++ src/modules/Dashboard.jsx
@@ -0,0 +1,11 @@
+import '../style/scss/admin/_dashboard.scss'
+
+const Dashboard = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Dashboard
--- src/modules/Reservas.jsx
+++ src/modules/Reservas.jsx
@@ -1,11 +1,7 @@
-import HeaderReservas from "../components/reserves/HeaderReservas"
-import BannerReserves from "../components/reserves/BannerReserves"
-
 const Reservas = () => {
     return (
         <>
-            <HeaderReservas />
-            <BannerReserves />
+            
         </>
     )
 }
